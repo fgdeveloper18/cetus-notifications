24ef3bdb39fab22914a3253b0282c69f2da54036
Francys Garcia <francys@poincenot.com>
2024-12-29 17:28:58 -0300

    add spring boot project

---
+++ src/main/java/com/fmgarcia/cetus/notifications/CetusNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.fmgarcia.cetus.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CetusNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CetusNotificationsApplication.class, args);
+	}
+
+}
